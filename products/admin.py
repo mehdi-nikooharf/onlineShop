69d82e82a0f24d2a7bdd3e64888fcdd0ac81405b
unknown <www.mnikooharf@gmail.com>
2023-07-17 18:45:36 +0330

    add category to products.admin

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Category
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['parent', 'title', 'is_enable', 'created_time']
+    list_filter = ['is_enable', 'parent']
+    search_fields = ['title']
