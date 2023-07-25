c138d600b0212a34a722d36b27c7836a8dfe4f92
unknown <www.mnikooharf@gmail.com>
2023-07-25 20:00:19 +0330

    add avatar to categories admin

--- products/admin.py
+++ products/admin.py
@@ -5,6 +5,6 @@ from .models import Category
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ['parent', 'title', 'created_time']
+    list_display = ['parent', 'title', 'created_time', 'avatar']
     list_filter = ['parent']
     search_fields = ['title']
