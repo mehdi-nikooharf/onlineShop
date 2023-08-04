fa33268ece0650f4aead85fe0836f2f5681c48c6
unknown <www.mnikooharf@gmail.com>
2023-08-04 16:05:29 +0330

    add name to account register url

--- account/urls.py
+++ account/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import url
 from django.urls import path, include
 from .api import RegisterApi
 urlpatterns = [
-      path('register', RegisterApi.as_view()),
+      path('register', RegisterApi.as_view(), name='account-register'),
 ]
